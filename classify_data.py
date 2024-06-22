5c95c24902c6b37344887a0741ca513b8fc661d1
oscar <oscar.pang050906@gmail.com>
2024-06-22 02:42:50 -0400

    classified data

---
+++ classify_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import torch
+from transformers import pipeline
+
+def classify_comment(comment):
+    result = classifier(comment, labels)
+    return result['labels'][0]
+
+input_file = 'comments.csv'
+comments_df = pd.read_csv(input_file)
+
+print(comments_df.head())
+
+classifier = pipeline('zero-shot-classification', model="facebook/bart-large-mnli")
+labels = ["informative", "frustrated", "afraid", "skeptical", "critical", "supportive", "concerned", "unconcerned", "optimistic"]
+
+comments_df['Type'] = comments_df['Comment'].apply(classify_comment)
+
+output_file = 'classified_data.csv'
+comments_df.to_csv(output_file, index=False)
