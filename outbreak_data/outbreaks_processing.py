24f16b037f04521d570ffb6e9489140eaf4e5207
oscar <oscar.pang050906@gmail.com>
2024-06-28 13:02:57 -0400

    updated graphs

--- outbreak_data/outbreaks_processing.py
+++ outbreak_data/outbreaks_processing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv('csv_files/outbreaks.csv', encoding='utf-16')
+data = pd.read_csv('csv_files/outbreaks.csv')
 
 data['Week'] = pd.to_datetime(data['Date'], format='mixed').dt.to_period('W')
 
