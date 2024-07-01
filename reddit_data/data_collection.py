ee01fc216ce669d421c94a2eb52c9ab7ffaaf1b4
oskip0906 <oscar.pang050906@gmail.com>
2024-07-01 13:17:57 -0400

    location based reddit data collection

--- reddit_data/data_collection.py
+++ reddit_data/data_collection.py
@@ -1,49 +1,77 @@
 import requests
-import re
-import csv
+import pandas as pd
 from datetime import datetime
 
-comments_link = 'https://api.pullpush.io/reddit/search/comment/?'
+link_structure = 'https://api.pullpush.io/reddit/search/comment/?'
 
-parameters = {
-    'q': 'H5N1',
+param_structure = {
     'sort': 'asc',
-    'sort_type': 'created_utc',
     'size': 100,
-    'after': int(datetime.strptime('2022-01-01', "%Y-%m-%d").timestamp())
+    'after': int(datetime.strptime('2022-02-08', "%Y-%m-%d").timestamp())
 }
 
-for key, value in parameters.items():
-    comments_link += f'{key}={value}&'
+bird_flu = {
+    'q': '"bird%20flu"',
+    'param_structure': param_structure
+}
 
-# 100 comments max per request
+avian_flu = {
+    'q': '"avian%20flu"',
+    'param_structure': param_structure
+}
 
-link_1 = f'{comments_link}subreddit=H5N1_AvianFlu'
-link_2 = f'{comments_link}subreddit=epidemiology'
-link_3 = f'{comments_link}subreddit=medicine'
-link_4 = f'{comments_link}subreddit=science'
-link_5 = f'{comments_link}subreddit=news'
-link_6 = f'{comments_link}subreddit=worldnews'
-link_7 = f'{comments_link}subreddit=Health'
-link_8 = f'{comments_link}subreddit=OutOfTheLoop'
+avian_influenza = {
+    'q': '"avian%20influenza"',
+    'param_structure': param_structure
+}
 
-links = [link_1, link_2, link_3, link_4, link_5, link_6, link_7, link_8]
+h5n1 = {
+    'q': 'h5n1',
+    'param_structure': param_structure
+}
 
-print(links)
+parameters = [bird_flu, avian_flu, avian_influenza, h5n1]
 
-with open('csv_files/comments.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Comment', 'Date'])
-    for link in links:
-        comment_response = requests.get(link)
-        comments = comment_response.json()
-        count = 0
-        for comment in comments['data']:
-            try:
-                date = datetime.fromtimestamp(comment['created_utc']).strftime('%Y-%m-%d %H:%M:%S')
-                encoded_body = comment['body'].encode('latin-1', errors='replace')
-                writer.writerow([encoded_body.decode('latin-1'), date])
-                count += 1
-            except:
-                continue
-        print(count)
+comments_links = []
+
+for parameter in parameters:
+    param_structure = parameter['param_structure']
+    param_query = parameter['q']
+    comments_links.append(f'{link_structure}q={param_query}&{"&".join(f"{key}={value}" for key, value in param_structure.items())}')
+
+print(comments_links)
+
+subreddits = ['minnesota', 'southdakota', 'california', 'pennsylvania', 'iowa', 'colorado', 'wisconsin', 
+              'michigan', 'ohio', 'texas', 'washington', 'utah', 'maryland', 'kansas']
+
+links = []
+
+for subreddit in subreddits:
+    for link in comments_links:
+        links.append(f'{link}&subreddit={subreddit}')
+
+# print(links)
+
+comments_data = []
+
+for link in links:
+    comment_response = requests.get(link)
+    comments = comment_response.json()
+    count = 0
+    for comment in comments['data']:
+        try:
+            date = datetime.fromtimestamp(comment['created_utc']).strftime('%Y-%m-%d %H:%M:%S')
+            subreddit = link.split('&')[-1].split('=')[-1]
+            comments_data.append({'Comment': comment['body'], 'Date': date, 'Subreddit': subreddit})
+            count += 1
+        except Exception as e:
+            print(f"error processing comment")
+            continue
+    print(count)
+
+df = pd.DataFrame(comments_data)
+
+print(df)
+
+new_df = df.drop_duplicates(subset=['Comment'])
+new_df.to_csv('csv_files/comments.csv', index=False)
